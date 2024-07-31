2b7e6ecfe73d7db211447bc728277c3fea74b682
Auronus <auronus@mail.ru>
2024-07-31 14:22:20 +0300

    Creational HW. Task 1

---
+++ src/PersonBuilder.java
@@ -0,0 +1,43 @@
+import java.util.OptionalInt;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private OptionalInt age;
+    private String address;
+
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст меньше 0");
+        }
+        this.age = OptionalInt.of(age);
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        Person person;
+        if (age != null && age.isPresent()) {
+            person = new Person(name, surname, age.getAsInt());
+        } else {
+            person = new Person(name, surname);
+        }
+        person.setAddress(address);
+        return person;
+    }
+}
